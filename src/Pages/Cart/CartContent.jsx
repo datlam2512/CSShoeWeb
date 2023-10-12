d27ebd552870759ddadc00f77b8959d811e88018
timboace0 <122100456+timboace0@users.noreply.github.com>
2023-10-12 15:04:20 +0700

    empty cart notify

--- src/Pages/Cart/CartContent.jsx
+++ src/Pages/Cart/CartContent.jsx
@@ -21,11 +21,18 @@ export default function CartContent() {
         })}
         </div>
 
+        {totalAmount > 0 ? (
         <div className='checkout'>
           <p>Subtotal: {totalAmount}</p>
           <button onClick={() => navigate("/")}> Continue Shopping </button>
           <button> Checkout </button>
-        </div>
+        </div> 
+        ) : (
+          <div>
+          <h2> Your Cart Is Empty!</h2>
+          <button onClick={() => navigate("/")}> Continue Shopping </button>
+          </div>
+        )}
     </div>  
   )
 }
