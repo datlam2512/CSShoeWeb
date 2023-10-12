35620b41c92163123734bcfe2d15b31d1f0a005b
mtphuc253 <mtp25032003@gmail.com>
2023-10-12 14:29:03 +0700

    update ProductDetail update string price to integer in ProductList

--- src/Pages/ShopPage/ProductMarketPlace.jsx
+++ src/Pages/ShopPage/ProductMarketPlace.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import './ProductMarketPlace.css'
 import Product from './Product';
-import products  from './ProductList';
+import products from './ProductList';
 export default function ProductMarketPlace() {
+    
     return (
         <div className='row'>
             {products.map((product) => (
+                
                 <Product
                     key={product.id}
                     id={product.id}
                     imgUrl={product.imgUrl}
                     name={product.name}
-                    price={product.price}
+                    price={product.price.toLocaleString() + " VNĐ"}
                 />
             ))}
         </div>
