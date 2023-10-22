7ad81b063ccebdf5e280cddc6848c7558c42cd0a
datlam2512 <144305364+datlam2512@users.noreply.github.com>
2023-10-22 23:08:49 +0700

    update blogs, search navbar
    update blogs detail and searchbar into navbar

--- src/Pages/General/Home.jsx
+++ src/Pages/General/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../Home/Header'
 import Main from '../Home/Main'
+import Search from './Search'
+import SearchResult from './SearchResult'
 export default function Home() {
   return (
     <div>
         <Header/>
         <Main/>
-    
     </div>
   )
 }
