9dbfbc4e2c9b9f6a63c641c603ff807e28de4aa3
datlam2512 <144305364+datlam2512@users.noreply.github.com>
2023-10-26 00:28:31 +0700

    fixed
    fixed

--- src/Account/AccountPage.jsx
+++ src/Account/AccountPage.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../context/user-context'
 import { Navigate, useNavigate } from 'react-router-dom';
-
+import './AccountPage.css'
 function AccountPage() {
     const { setUser } = useContext(UserContext);
     const navigate = useNavigate();
@@ -10,7 +10,7 @@ function AccountPage() {
         navigate('/Login');
     }
     return (
-    <div>
+    <div className='account-page'>
         <div>AccountPage</div>
         <button onClick={handleLogout}>Logout</button>
     </div>
