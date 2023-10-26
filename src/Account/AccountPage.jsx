83912bd8d0dc64861b8b186581bc543147751cf3
datlam2512 <144305364+datlam2512@users.noreply.github.com>
2023-10-26 14:38:00 +0700

    update account page
    update account page

--- src/Account/AccountPage.jsx
+++ src/Account/AccountPage.jsx
@@ -1,39 +1,44 @@
-import React, { useContext } from 'react'
-import { UserContext } from '../context/user-context'
-import { Link, Navigate, useNavigate } from 'react-router-dom';
-import './AccountPage.css'
-import { loginUser } from '../Pages/Login/api';
+import React, { useContext } from "react";
+import { UserContext } from "../context/user-context";
+import { Link, Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import "./AccountPage.css";
+import { loginUser } from "../Pages/Login/api";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Accountdetail from "./Accountdetail";
 function AccountPage() {
-    const { user, setUser } = useContext(UserContext);
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        setUser(null); //clear user tu context
-        navigate('/Login');
-    }
-    return (
-    <div className='account-page'>
-        <div className='subnav-account'>
-        <div className='subnav-top'>
-        <i><FontAwesomeIcon icon={faUserCircle} /></i>   
-        <p>{user?user.Username:""}</p>
+  const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    setUser(null); //clear user tu context
+    navigate("/Login");
+  };
+  return (
+    <div className="account-page">
+      <div className="subnav-account">
+        <div className="subnav-top">
+          <i>
+            <FontAwesomeIcon icon={faUserCircle} />
+          </i>
+          <p>{user ? user.Username : ""}</p>
         </div>
         <ul>
-            <li className='account-detail'><Link to={'#accountdetail'}>Account detail</Link></li>
-            <li className='account-detail'><a href='#Adress'>Address</a></li>
-            <li className='account-detail'><a href='#historypurchase'>History Purchase</a></li>
+          <li className="account-detail">
+            <Link to={"#accountdetail"}>Account detail</Link>
+          </li>
+          <li className="account-detail">
+            <Link to={"#addressship"}>Address detail</Link>
+          </li>
+          <li className="account-detail">
+            <Link to={"#purchasehistory"}>Purchase History</Link>
+          </li>
         </ul>
         <button onClick={handleLogout}>Logout</button>
-        </div>
-        <div className='account-content'>
-            <p>content</p>
-        </div>
-        <div>
-
-        </div>
+      </div>
+      <div className="account-content"></div>
+      <div></div>
     </div>
-  )
+  );
 }
 
 export default AccountPage;
