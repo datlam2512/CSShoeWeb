8d9ed9f515dd376a6e5ca1fe95782590ac38395c
datlam2512 <144305364+datlam2512@users.noreply.github.com>
2023-10-30 13:57:34 +0700

    update contact
    update contact to reset form after send email

--- src/Pages/Contact/Content.jsx
+++ src/Pages/Contact/Content.jsx
@@ -1,40 +1,38 @@
-import React,{ useRef } from 'react'
+import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-import './Contact.css'
-export default function 
-() {
+import './Contact.css';
+
+export default function Contact() {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-
     emailjs.sendForm('service_an92sag', 'template_kwiayh5', form.current, 'MfcBNeLDL7S9m3IiC')
       .then((result) => {
-          console.log(result.text);
-          console.log("message sent");
+        console.log(result.text);
+        console.log("message sent");
+        form.current.reset(); // Reset the form
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
-      const SubmiReset=()=>{
-        document.getElementById("form-contact").reset();
-      }
   };
+
   return (
     <div id='form-contact'>
       <h1 className='Contact-h1'>Contact</h1>
       <div className='form-input'>
         <form ref={form} onSubmit={sendEmail}>
-      <label >Name</label>
-      <input className='input' type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Phone</label>
-      <input type="input" name="user_phone" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input onClick={SubmiReset} className='submit' type="submit" value="Send" />
-    </form>
+          <label>Name</label>
+          <input className='input' type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Phone</label>
+          <input type="input" name="user_phone" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input className='submit' type="submit" value="Send" />
+        </form>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
