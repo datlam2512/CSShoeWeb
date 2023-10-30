e3eaecfec1ad5f4220b0393f146754713d573fc2
datlam2512 <144305364+datlam2512@users.noreply.github.com>
2023-10-30 13:39:37 +0700

    update contact
    update form contact

--- src/Pages/Contact/Content.jsx
+++ src/Pages/Contact/Content.jsx
@@ -8,16 +8,19 @@ export default function
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_an92sag', 'template_fvx71ed', form.current, 'MfcBNeLDL7S9m3IiC')
+    emailjs.sendForm('service_an92sag', 'template_kwiayh5', form.current, 'MfcBNeLDL7S9m3IiC')
       .then((result) => {
           console.log(result.text);
           console.log("message sent");
       }, (error) => {
           console.log(error.text);
       });
+      const SubmiReset=()=>{
+        document.getElementById("form-contact").reset();
+      }
   };
   return (
-    <div className='form-contact'>
+    <div id='form-contact'>
       <h1 className='Contact-h1'>Contact</h1>
       <div className='form-input'>
         <form ref={form} onSubmit={sendEmail}>
@@ -25,9 +28,11 @@ export default function
       <input className='input' type="text" name="user_name" />
       <label>Email</label>
       <input type="email" name="user_email" />
+      <label>Phone</label>
+      <input type="input" name="user_phone" />
       <label>Message</label>
       <textarea name="message" />
-      <input className='submit' type="submit" value="Send" />
+      <input onClick={SubmiReset} className='submit' type="submit" value="Send" />
     </form>
     </div>
     </div>
