ff37b3dc7d95061a969329093feae4326380c450
datlam2512 <144305364+datlam2512@users.noreply.github.com>
2023-11-06 17:45:42 +0700

    fixed
    fixed

--- src/Pages/BlogPages/BlogContent.jsx
+++ src/Pages/BlogPages/BlogContent.jsx
@@ -1,37 +1,21 @@
-import React, { useEffect, useState } from "react";
-import Blogs from "./ListOfBlog";
-import "./Blog.css";
-import { Link } from "react-router-dom";
-import { http } from "../../config/http";
-
+import React, { useState } from 'react'
+import Blogs from './ListOfBlog'
+import './Blog.css'
+import { Link } from 'react-router-dom';
 export default function BlogContent() {
   const [blogs, setbBlog] = useState([]);
-
-  const fetchAllBlogs = async () => {
-    const data = (await http.get("/blogs/")).data;
-    setbBlog(data);
-  };
-
-  useEffect(() => {
-    fetchAllBlogs();
-  }, []);
-
   return (
     <div>
-      <div className="Container">
-        <div className="row blog-content">
-          {blogs.map(blogs => {
-            return (
-              <div className="col-lg-4 col-md-5 col-sm-6 card-blog">
-                <img src={blogs.img} />
-                <Link to={`/blog-detail/${blogs.id}`}>
-                  <p className="infor-content">{blogs.title}</p>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+      <div className='Container'>
+        <div className='row blog-content'>  
+   {Blogs.map((blogs)=>(
+    <div className='col-lg-4 col-md-5 col-sm-6 card-blog'>
+    <img src={blogs.img}/>
+    <Link to={`/blog-detail/${blogs.id}`} ><p className='infor-content'>{blogs.tittle}</p></Link>
     </div>
-  );
+   ))}
+   </div>
+   </div>
+    </div>
+  )
 }
--- src/Pages/General/Navshoes.jsx
+++ src/Pages/General/Navshoes.jsx
@@ -5,28 +5,41 @@ import { Link, useNavigate } from "react-router-dom";
 import "./Navigationshoe.css";
 import { NavItem, Navbar, Icon, Tabs, Tab } from "react-materialize";
 import { AudioOutlined } from "@ant-design/icons";
-import { Button, Dropdown, Input, Space } from "antd";
+import { Button, Dropdown, Input, Space, Menu } from "antd";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SearchBar from "./SearchBar";
 import { UserContext } from "../../context/user-context";
-import { useSelector } from "react-redux";
+import { DownOutlined } from '@ant-design/icons';
 
 export default function () {
-  const { user } = React.useContext(UserContext);
+  const { user, setUser } = React.useContext(UserContext);
   const navigate = useNavigate();
   const { Search } = Input;
   const onSearch = (value, _e, info) => console.log(info?.source, value)
-  const currentUser = useSelector((state) => state.user.currentUser)
-
   const handleClick = () => {
-    if (currentUser.token) {
+    if (user) {
       navigate('/Account')
     } else {
       navigate('/Login')
     }
   };
-
+  const handleLogout = () => {
+    sessionStorage.removeItem('email');
+    sessionStorage.removeItem('phone');
+    setUser(null);
+    navigate("/Login");
+  }
+  const menu = (
+    <Menu style={{backgroundColor:'black'}}>
+      <Menu.Item key="1" onClick={handleClick} style={{color:'white', fontFamily:'Unica One,sans-serif', fontSize:'larger'}}>
+        Profile
+      </Menu.Item>
+      <Menu.Item key="2" onClick={handleLogout} style={{color:'white', fontFamily:'Unica One,sans-serif', fontSize:'larger'}}>
+        Logout
+      </Menu.Item>
+    </Menu>
+  );
   return (
     <div className="Navbar">
       <div className="Navbarheader">
@@ -34,21 +47,21 @@ export default function () {
           <img src="/Img/logo6.png" />
         </Link>
         <div className="Navbarbottom">
-          <ul className="menu">
-            <li>
-              <Link
-                className="link-nav"
-                to="/"
-                style={{ textDecoration: "none", color: "#dbdbdb" }}
-              >
-                Home
-              </Link>
-            </li>
+        <ul className="menu">
+          <li>
+            <Link
+              className="link-nav"
+              to="/"
+              style={{ textDecoration: "none", color: "#dbdbdb" }}
+            >
+              Home
+            </Link>
+          </li>
 
-            <li>
-              <Space direction="vertical">
-                <Space wrap>
-
+          <li>
+            <Space direction="vertical">
+              <Space wrap>
+                
                   <Link
                     className="link-nav"
                     to="/shop"
@@ -56,84 +69,79 @@ export default function () {
                   >
                     Shop
                   </Link>
-
-                </Space>
+                
               </Space>
-            </li>
+            </Space>
+          </li>
 
-            <li>
-              <Link
-                className="link-nav"
-                to="/createyourown"
-                style={{ textDecoration: "none", color: "#dbdbdb" }}
-              >
-                Create Your Own
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="link-nav"
-                to="/about"
-                style={{ textDecoration: "none", color: "#dbdbdb" }}
-              >
-                About Us
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="link-nav"
-                to="/contact"
-                style={{ textDecoration: "none", color: "#dbdbdb" }}
-              >
-                Contact
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="link-nav"
-                to="/sizeguide"
-                style={{ textDecoration: "none", color: "#dbdbdb" }}
-              >
-                Size Guide
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="link-nav"
-                to="/blogs"
-                style={{ textDecoration: "none", color: '#dbdbdb' }}
-              >
-                Blog
-              </Link>
-            </li>
-            {
-              currentUser.isAdmin ? <li>
-                <Link
-                  className="link-nav"
-                  to="/admin"
-                  style={{ textDecoration: "none", color: '#dbdbdb' }}
-                >
-                  Admin
-                </Link>
-              </li>
-                : <></>
-            }
-          </ul>
+          <li>
+            <Link
+              className="link-nav"
+              to="/createyourown"
+              style={{ textDecoration: "none", color: "#dbdbdb" }}
+            >
+              Create Your Own
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="link-nav"
+              to="/about"
+              style={{ textDecoration: "none", color: "#dbdbdb" }}
+            >
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="link-nav"
+              to="/contact"
+              style={{ textDecoration: "none", color: "#dbdbdb" }}
+            >
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="link-nav"
+              to="/sizeguide"
+              style={{ textDecoration: "none", color: "#dbdbdb" }}
+            >
+              Size Guide
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="link-nav"
+              to="/blogs"
+              style={{ textDecoration: "none", color:'#dbdbdb' }}
+            >
+              Blog
+            </Link>
+          </li>
+        </ul>
         </div>
         <div className="search-cart-nav">
           <div className="search-bars">
-            <SearchBar />
+            <SearchBar/>
           </div>
           <button className="shopping-cart" type="submit">
             <Link to="/CartContent">
               <i class="fa fa-shopping-cart cart" aria-hidden="true"></i>
             </Link>
           </button>
-          <button className="Login" type="submit" onClick={handleClick}>
-            {" "}
-            <Link to="/Login">
-              <i class="fa fa-user-circle-o login" aria-hidden="true"></i>
-            </Link>
+          <button className="Login" type="submit" onClick={user ? null : handleClick}>
+            {user ? (
+              <Dropdown overlay={menu} trigger={['click']}>
+                <Link style={{ display: 'flex', alignItems: 'center' }}>
+                  <i class="fa fa-user-circle-o login" aria-hidden="true"></i> <DownOutlined />
+                </Link>
+              </Dropdown>
+            ) : (
+              <Link to="/Login">
+                <i class="fa fa-user-circle-o login" aria-hidden="true"></i>
+              </Link>
+            )}
           </button>
         </div>
       </div>
